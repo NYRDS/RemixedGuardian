fe27a8e999720bac8e4be661f8aa5630422226cf
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-03-09 16:45:12 +0300

    add mistral backend

---
+++ mistral_test.py
@@ -0,0 +1,30 @@
+
+from mistralai import Mistral, UserMessage
+from conf import MISTRAL_API_KEY
+
+client = Mistral(api_key=MISTRAL_API_KEY)
+
+def mistral_chat(prompt):
+    chat_response = client.chat.complete(
+        messages=prompt,
+        model="mistral-large-latest",
+        max_tokens=1024,
+        temperature=1,
+        top_p=1,
+    )
+    return chat_response.choices[0].message.content
+
+if __name__ == "__main__":
+    messages = [
+        {"role": "system", "content": "Ты крысиный король, всегда отвечаешь на русском языке. Ты очень заносчив и не слишком умён."},
+        {"role": "user", "content": "Как тебя зовут?"},
+    ]
+
+    response = client.chat.complete(
+        messages=messages,
+        model="mistral-large-latest",
+        max_tokens=1024,
+        temperature=1,
+        top_p=1,
+    )
+    print(response.choices[0].message.content)
